2be0b5f9019416682d9d63cc6f7c67da1e8d5802
Mac_Pro <luoximeimei@126.com>
2019-11-26 19:42:26 +0800

    DIUDIU is ready for drive a truck~

---
+++ R/Dif_gene.R
@@ -0,0 +1,15 @@
+library("DESeq2")
+cts.count <- read.delim("~/Documents/DrawPictureR/data/dfr_0.01.count.txt")
+condition <- factor(c("A.a","A.a","A.a","A.b","A.b","A.b",
+                      "B.a","B.a","B.a","B.b","B.b","B.b",
+                      "C.a","C.a","C.a","C.b","C.b","C.b",
+                      "CK","CK","CK"))
+colData <- data.frame(row.names=colnames(cts.count)[-1],condition)
+database <- as.matrix(cts.count[,-1])
+dds <- DESeqDataSetFromMatrix(countData = database, colData = colData, design= ~ condition)
+dds <- DESeq(dds)
+res <- results(dds)
+table(res$padj < 0.05)
+res <- res[order(res$padj),]
+resdata <- merge(as.data.frame(res), as.data.frame(counts(dds, normalized=TRUE)),by="row.names",sort=FALSE)
+write.csv(resdata,file = "test.csv")
